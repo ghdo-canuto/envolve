f073cfe6219b2a341549ce5ab11a5bac8f62ad42
Canuto <83791543+unixsupsys@users.noreply.github.com>
2022-12-01 00:00:23 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,39 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.33.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-azdo-terraform"
+    storage_account_name = "stgdevazdoterraform"
+    container_name       = "tfstate"
+    key                  = "terraformexample.tfstate"
   }
 }
-
+ 
 provider "azurerm" {
-  # Configuration options
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
+ 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.tamops.name
+}
+ 
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
 }
