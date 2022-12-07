4eecc9eb76f246cc0168f6f983310ac9d4dee7e3
Canuto <83791543+unixsupsys@users.noreply.github.com>
2022-12-07 16:46:37 -0300

    Update providers.tf

--- stacks/solo_network/providers.tf
+++ stacks/solo_network/providers.tf
@@ -4,8 +4,4 @@ provider "azurerm" {
      prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "#{ARM-SUBSCRIPTION-ID}#"
-  tenant_id       = "#{ARM-TENANT-ID}#"
-  client_id       = "#{ARM-CLIENT-ID}#"
-  client_secret   = "#{ARM-CLIENT-SECRET}#"
 }
